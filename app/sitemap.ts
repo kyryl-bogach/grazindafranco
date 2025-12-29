c1e0d3e446075038bdd4b9d4b35d84ec41018467
kyryl-bogach <kyryl.bogach@gmail.com>
2025-12-29 09:53:02 +0100

    feat: remove outdated URLs from sitemap for improved SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,30 +12,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: `${baseUrl}/sobre-mi`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/galeria`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/servicio-novias`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/contacto`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
   ];
 }
 
